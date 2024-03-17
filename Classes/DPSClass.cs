fd98297dee895e72f93316475de7da8772583a62
Mutrax <56598673+Mutraxation@users.noreply.github.com>
2024-03-17 13:27:34 +0100

    little bit of spawn mechanic

--- Classes/DPSClass.cs
+++ Classes/DPSClass.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript2 : BaseClass
+public class DPSClass : BaseClass
 {
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +15,11 @@ public class NewBehaviourScript2 : BaseClass
     void Update()
     {
         
+    }
+
+    void Spawn(float tempHealth, float[] tempDamage, float[] tempCoords)
+    {
+
+        BaseSpawn(tempHealth, tempDamage, tempCoords);
     }
 }
